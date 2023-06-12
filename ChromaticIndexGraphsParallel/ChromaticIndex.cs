3c3815ee2039e8454439b8216b0d32e1ff791503
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-06-12 13:46:22 +0400

    Coloring

--- ChromaticIndexGraphsParallel/ChromaticIndex.cs
+++ ChromaticIndexGraphsParallel/ChromaticIndex.cs
@@ -6,13 +6,12 @@ namespace ChromaticIndexGraphsParallel
 {
     class ChromaticIndex
     {
+        static object locker = new object();
         public int FindChromaticIndex(List<Edge> edges, int e)
         {
             //Parallel.For(0, e, x => FindChromInd(edges, e, x));
             
-            
-            Coloring(edges, 0, e, 10);
-            
+            Parallel.For(1, 10, x => Coloring(edges, 0, e, x));
 
             int max = 0;
 
@@ -42,30 +41,32 @@ namespace ChromaticIndexGraphsParallel
         private void Coloring(List<Edge> edges, int i, int m, int num_color)
         {
             if (i == m) return;
-            for (int c = 1; c <= num_color; c++)
+            for (int j = 0; j < m; j++)
             {
-                for (int j = 0; j < m; j++)
+                if (j == i)
+                    continue;
+                if ((edges[j].FirstVertex == edges[i].FirstVertex || edges[j].FirstVertex == edges[i].SecondVertex
+                || edges[j].SecondVertex == edges[i].FirstVertex || edges[j].SecondVertex == edges[i].SecondVertex)
+                && edges[j].Color == edges[i].Color)
                 {
-                    if (j == i)
-                        continue;
-                    if ((edges[j].FirstVertex == edges[i].FirstVertex || edges[j].FirstVertex == edges[i].SecondVertex
-                    || edges[j].SecondVertex == edges[i].FirstVertex || edges[j].SecondVertex == edges[i].SecondVertex)
-                    && edges[j].Color == edges[i].Color)
-                    {
-                        edges[i].Color = c;
-                        Coloring(edges, i+1, m, num_color);
-                    }
+                    edges[i].Color = num_color;
+                    Coloring(edges, i+1, m, num_color);
                 }
-                    
             }
+                    
+          
         }
 
         private void FindMaxColor(List<Edge> edges, int i, ref int max)
         {
-            if (max < edges[i].Color)
+            lock (locker)
             {
-                max = edges[i].Color;
+                if (max < edges[i].Color)
+                {
+                    max = edges[i].Color;
+                }
             }
+            
         }
 
     }
